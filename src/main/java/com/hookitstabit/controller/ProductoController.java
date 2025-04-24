a2668800fa48e12533a265c8e6007814234e9609
arp-10 <alejandrarodriguezp97@gmail.com>
2025-04-24 14:13:14 +0200

    Producto Controller creado

---
+++ src/main/java/com/hookitstabit/controller/ProductoController.java
@@ -0,0 +1,41 @@
+package com.hookitstabit.controller;
+
+import com.hookitstabit.dao.ProductoDAO;
+import com.hookitstabit.model.Producto;
+import jakarta.json.bind.Jsonb;
+import jakarta.json.bind.JsonbBuilder;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+@WebServlet("/api/productos")
+public class ProductoController extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private static final String APPLICATION_JSON = "application/json";
+    private static final Jsonb JSONB = JsonbBuilder.create();
+    private static final ProductoDAO DAO = new ProductoDAO();
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        response.setContentType(APPLICATION_JSON);
+        PrintWriter out = response.getWriter();
+
+        List<Producto> productos = DAO.obtenterProductos();
+                String json = JSONB.toJson(productos);
+
+        out.write(json);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+}
